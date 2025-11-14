8edbc404b6aa78cfded293c473962e5ca8dc68db
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-11-13 21:32:56 -0300

    construindo category model

---
+++ src/models/categoryModel.js
@@ -0,0 +1,20 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Category = sequelize.define("Category", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  //continua proxima aula...
+});
